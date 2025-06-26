fd321a31118199fb941c9b67682a4a03499136b7
shkamiya <mathmrk.ggl@gmail.com>
2025-06-27 06:21:43 +0900

    small modif

--- job_scripts/train_classification.sh
+++ job_scripts/train_classification.sh
@@ -14,11 +14,11 @@ cd /work/gb20/b20109/akorn  # 実際のパスに合わせて変更
 
 # SingularityでPythonスクリプト実行
 singularity exec --nv --bind $(pwd):/workspace singularity/akorn_wisteria.sif \
-  python train_classification.py cifar10_test \
+  python train_classification.py miyato_cifar10_test \
     --data cifar10 \
     --epochs 100 \
     --batchsize 64 \
     --n 2 \
     --ch 32 \
-    --L 4 \
+    --L 3 \
     --T 8
