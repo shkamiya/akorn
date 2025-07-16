0cf1ee4920a6e5530edaf67e52355229576ae970
shkamiya <shunsuke.kamiya@weblab.t.u-tokyo.ac.jp>
2025-07-16 11:41:30 +0900

    sweep code added

--- job_scripts/submit_all_cifar10_myakorn_classification_sweep.sh
+++ job_scripts/submit_all_cifar10_myakorn_classification_sweep.sh
@@ -1,3 +1,3 @@
-for i in $(seq 0 2); do
-  qsub -v INDEX=$i job_scripts/cifar10_myakorn_classification_sweep.pbs
+for i in $(seq 0 11); do
+  qsub -v INDEX=$i job_scripts/cifar10_myakorn_small_kernel_sweep.pbs
 done
