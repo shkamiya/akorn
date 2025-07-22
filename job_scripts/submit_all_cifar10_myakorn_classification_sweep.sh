b21e63b69ba125586f543ddcef7dda56e346e26c
shkamiya <shunsuke.kamiya@weblab.t.u-tokyo.ac.jp>
2025-07-22 15:16:59 +0900

    wisteria modified

--- job_scripts/submit_all_cifar10_myakorn_classification_sweep.sh
+++ job_scripts/submit_all_cifar10_myakorn_classification_sweep.sh
@@ -1,3 +1,3 @@
 for i in $(seq 0 0); do
-  pjsub INDEX=$i job_scripts/cifar10_akorn_resnet_slim_classification_L1T0_sweep.pjm
+  pjsub -x INDEX=$i job_scripts/cifar10_akorn_resnet_slim_classification_L1T0_sweep.pjm
 done
